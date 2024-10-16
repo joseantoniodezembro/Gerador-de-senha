9097902fde49623375f004154c7e384699ef77de
José Antonio Dezembro <108530292+joseantoniodezembro@users.noreply.github.com>
2024-10-16 20:52:17 -0300

    Add files via upload

---
+++ Gerador de senha/script.js
@@ -0,0 +1,25 @@
+let slideElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("password");
+
+let containerPassword = document.querySelector("container-password");
+
+let charset ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@";
+let novaSenha = "";
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+    let pass = "";
+
+    for(let i = 0, n = charset.length; i < slideElement.value; ++i){
+pass += charset.charAt(Math.floor(Math.random() *n));
+
+    }
+   password.innerHTML = pass;
+   containerPassword.classList.remove("hide");
+}
